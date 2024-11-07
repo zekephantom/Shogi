01a20c9100373ff1038ac075545f45892383e149
arnaj <sandhu26@up.edu>
2024-11-07 14:22:53 -0800

    finished lab 08 tests

--- app/src/test/java/edu/up/cs301/Shogi/ShogiStateTest.java
+++ app/src/test/java/edu/up/cs301/Shogi/ShogiStateTest.java
@@ -8,10 +8,27 @@ import org.junit.Test;
 
 public class ShogiStateTest {
 
+    ShogiState firstInstance = new ShogiState(); // Create new instance of game state class
+    ShogiHumanPlayer player = new ShogiHumanPlayer("player1");
+    ShogiMoveAction movePawn1;
+    ShogiMoveAction moveGoldG1;
+    ShogiMoveAction moveBishop1;
+    ShogiMoveAction moveGoldG2;
+    ShogiMoveAction moveBishop2;
+    ShogiMoveAction moveKing;
+    ShogiMoveAction dropGoldG;
+
     @Before
     public void setUp() throws Exception {
-
-
+        movePawn1 = new ShogiMoveAction(player, firstInstance.getPiece(6, 2), 5, 2);
+        moveGoldG1 = new ShogiMoveAction(player, firstInstance.getPiece(0, 3), 1, 2);
+        moveBishop1 = new ShogiMoveAction(player, firstInstance.getPiece(7, 1), 2, 6);
+        firstInstance.getPiece(2,6).bePromoted(true); // bishop promotion
+        moveGoldG2 = new ShogiMoveAction(player, firstInstance.getPiece(0, 5), 1, 5);
+        moveBishop2= new ShogiMoveAction(player, firstInstance.getPiece(2, 6), 1, 5);
+        moveKing = new ShogiMoveAction(player, firstInstance.getPiece(0, 4), 0, 3);
+        firstInstance.getPieces().get(35).setOnBoard(true);
+        dropGoldG = new ShogiMoveAction(player, firstInstance.getPieces().get(35), 1, 4);
     }
 
     @After
@@ -23,25 +40,17 @@ public class ShogiStateTest {
 
     @Test
     public void moveAction() {
-        ShogiState firstInstance = new ShogiState(); // Create new instance of game state class
-        //ShogiState firstCopy = new ShogiState(firstInstance); 	//Create deep copy from player 1's perspective
-        ShogiHumanPlayer player = new ShogiHumanPlayer("player1");
-
-        ShogiMoveAction movePawn1 = new ShogiMoveAction(player, firstInstance.getPiece(6, 2), 5, 2);
         assertTrue(firstInstance.moveAction(movePawn1));
-
-        ShogiMoveAction movePawn2 = new ShogiMoveAction(player, firstInstance.getPiece(2, 2), 5, 2);
-        assertFalse(firstInstance.moveAction(movePawn2));
     }
 
     @Test
     public void dropAction() {
-
+        assertTrue(firstInstance.moveAction(dropGoldG);
     }
 
     @Test
     public void promoteAction() {
-
+        assertTrue(firstInstance.getPiece(1, 5).isPromoted());
     }
 
 }
